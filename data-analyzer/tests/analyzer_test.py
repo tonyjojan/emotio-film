465f6c4419f3e1866d25502b6efdddb215861efd
Tony Jojan <jojan@Tonys-MBP-2.hsd1.ca.comcast.net>
2023-12-12 11:49:01 -0800

    commented UT

--- data-analyzer/tests/analyzer_test.py
+++ data-analyzer/tests/analyzer_test.py
@@ -1,14 +1,14 @@
-import json
-def test_true(app,client):
-    assert 1 == 1
+# import json
+# def test_true(app,client):
+#     assert 1 == 1
 
-# def test_endpt(app,client):
-#     rv = client.get('/test_endpoint')
-#     assert b'alive' in rv.data
+# # def test_endpt(app,client):
+# #     rv = client.get('/test_endpoint')
+# #     assert b'alive' in rv.data
 
-# def test_get_movies(app,client):
-#     rv = client.get('movies')
-#     assert b'Rated English Movies' in rv.data
+# # def test_get_movies(app,client):
+# #     rv = client.get('movies')
+# #     assert b'Rated English Movies' in rv.data
 
 
     
