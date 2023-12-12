480d48864a3a786ce9fd44069018e85a80c640da
Tony Jojan <jojan@Tonys-MBP-2.hsd1.ca.comcast.net>
2023-12-12 11:47:47 -0800

    commented out tests

--- data-analyzer/tests/conftest.py
+++ data-analyzer/tests/conftest.py
@@ -1,22 +1,24 @@
-import pytest
-import os
-import sys
-import sqlite3
-topdir = os.path.join(os.path.dirname(__file__), "..")
-sys.path.append(topdir)
-from src.app import app as flask_app
+# import pytest
+# import os
+# import sys
+# import sqlite3
 
-@pytest.fixture
-def app():
-    yield flask_app
+# topdir = os.path.join(os.path.dirname(__file__), "..")
 
-@pytest.fixture
-def client():
-    return flask_app.test_client()
+# sys.path.append(topdir)
+# from src.app import app as flask_app
 
-@pytest.fixture
-def session(): # 1
-    connection = sqlite3.connect("database1.db")
-    db_session = connection.cursor()
-    yield db_session
-    connection.close()
+# @pytest.fixture
+# def app():
+#     yield flask_app
+
+# @pytest.fixture
+# def client():
+#     return flask_app.test_client()
+
+# @pytest.fixture
+# def session(): # 1
+#     connection = sqlite3.connect("database1.db")
+#     db_session = connection.cursor()
+#     yield db_session
+#     connection.close()
