ebdca98114f45150bb56b7566e26f7a191f293da
Tony Jojan <jojan@Tonys-MBP-2.hsd1.ca.comcast.net>
2023-12-12 12:31:17 -0800

    added nullcheck

--- ui-backend/src/app.py
+++ ui-backend/src/app.py
@@ -33,13 +33,15 @@ def load_movies(url, type):
     movie_list.append(type)
     print("url: "+ url)
     r = requests.get(url)
-    print("r" + r)
     json_r = r.json()
-    for element in json_r:
-        movie_list.append(element[0])
-    for element in movie_list:
-        print(element)
-    return render_template('view.html', input=movie_list)
+    if json_r == '[]':
+        return render_template('view.html', input=movie_list)
+    else: 
+        for element in json_r:
+            movie_list.append(element[0])
+        for element in movie_list:
+            print(element)
+        return render_template('view.html', input=movie_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9893)
